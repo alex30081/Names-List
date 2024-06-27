6db63b03ac1e3e820e43ef8487da8480a3c42812
Boichenko Oleksandr <alex.homerepair.www@gmail.com>
2024-06-27 13:44:52 +0100

    work with a list name through collections

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -2,25 +2,23 @@ package app;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
-import app.DataRepository;
+
 public class DataHandler {
 
-    // Метод формує виведення імені за певним індексом
     public String formOutput(List<String> list, int index) {
         try {
-            String name = list.getClass(DataRepository);
+            String name = list.get(2);
             return "Name: " + name + " is in index " + index;
         } catch (IndexOutOfBoundsException e) {
             return "Wrong index!";
         }
     }
 
-    // Метод формує виведення нумерованого списку імен
     public String formListOutput(List list) {
         StringBuilder sb = new StringBuilder();
         AtomicInteger count = new AtomicInteger(1);
-        for (name : ) {
-            sb.append(String.("%d) %s%n",
+        for (Object name : list) {
+            sb.append(String.format("%d) %s%n",
                     count.getAndIncrement(), name));
         }
         return "\nNames:\n" + sb;
--- src/app/DataRepository.java
+++ src/app/DataRepository.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataRepository {
-    // Метод повертає список імен
     public List<String> getData() {
         List<String> list = new ArrayList <>();
         list.add("Alice");
